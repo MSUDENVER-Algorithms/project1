53713f3be44bae32211ef0379ca600b68acaaebc
snubeagle <44102947+snubeagle@users.noreply.github.com>
2020-01-30 13:26:48 -0700

    Add files via upload

--- counter.java
+++ counter.java
@@ -1,5 +1,29 @@
 package algorithms_1;
 
+import java.util.ArrayList;
+
 public class counter {
+	public ArrayList<String> ones(ArrayList<String> a, int n){
+		String orig = "";
+		
+		if (!a.isEmpty()) {
+			orig = a.get(0);
+		}
+		
+		if (n == 0) {
+			return a;
+		}
+		else if(n == 1) {
+			orig = orig + "1";
+			a.set(0, orig);
+			ones(a, (n-1));
+		}
+		else {
+			orig = orig + "1+";
+			a.set(0, orig);
+			ones(a, (n-1));
+		}
+		return a;
+	}
 
 }
--- main.java
+++ main.java
@@ -1,5 +1,16 @@
 package algorithms_1;
 
+import java.util.ArrayList;
+
 public class main {
+	public static void main(String[] args) {
+		counter c = new counter();
+		ArrayList<String> list = new ArrayList<String>();
+		
+		
+		list.add("");
+		list = c.ones(list, 5);
+		System.out.println(list.get(0));
+	}
 
 }
