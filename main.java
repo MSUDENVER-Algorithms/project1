8e9690e6ad42fa55bb79a816ab14d478b8fba768
snubeagle <44102947+snubeagle@users.noreply.github.com>
2020-02-19 15:16:18 -0700

    Add files via upload

--- main.java
+++ main.java
@@ -1,21 +1,40 @@
 package algorithms_1;
-
-import java.util.ArrayList;
+import java.util.Scanner;
 
 public class main {
 	public static void main(String[] args) {
-		counter c = new counter();
-		ArrayList<String> list = new ArrayList<String>();
+		int[] out = new int[100];
+		Scanner sc = new Scanner(System.in);
+		int n;
 		
+		System.out.println("Enter number (n) you want to find the tree for:");
+		n = sc.nextInt();
 		
-		list.add("");
-		list = c.ones(list, 10);
-		list = c.collapser(list);
+		while (n <= 0) {
+			System.out.println("Invalid entry, please enter a valid n");
+			n = sc.nextInt();
+		}
 		
-		for(int i=0; i < list.size(); i++) {
-			System.out.println("Item " + i + ":");
-			System.out.println(list.get(i));
+		nxtLeaf(1, n, out, 0);
+	}
+	
+	public static void nxtLeaf(int i, int n, int[] out, int newIndex){
+		int j;
+		
+		if (n == 0) {
+			for(int k=0; k < newIndex-1; k++) {
+				System.out.print(out[k]);
+				System.out.print(" + ");
+				
+			}
+			System.out.print(out[newIndex-1] + "\n");
+		}
+		
+		for (j=i; j <= n; j++) {
+			out[newIndex] = j;
+			nxtLeaf(j, n-j, out, newIndex+1);
 		}
 	}
-
 }
+
+
